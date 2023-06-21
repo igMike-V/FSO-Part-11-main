4f9e2e7fdb733f93a3885a2c0db33e139b76bf78
Mike Vautour <mike@iglab.dev>
2023-06-21 15:25:32 -0400

    fixed environment variable import

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    dotenv(),
+    dotenv,
     react(),
   ],
 })
